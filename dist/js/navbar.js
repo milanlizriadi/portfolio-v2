ab4db3fb996f8d34c17392bb30a664e670e355c8
Milan Lizriadi <milanlizriadi@gmail.com>
2024-10-12 04:41:45 +0700

    Remove Navbar, Add Footer

--- dist/js/navbar.js
+++ dist/js/navbar.js
@@ -1,23 +1,18 @@
-const mobileMenuButton = document.getElementById("mobile-menu-button");
-const mobileMenu = document.getElementById("mobile-menu");
-let isOpen = false;
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-// Initially set the mobile menu to be hidden
-mobileMenu.classList.add("hidden");
-
-mobileMenuButton.addEventListener("click", () => {
-  isOpen = !isOpen;
-
-  if (isOpen) {
-    mobileMenu.classList.remove("hidden", "scale-y-0", "opacity-0");
-    mobileMenu.classList.add("scale-y-100", "opacity-100");
-  } else {
-    mobileMenu.classList.add("scale-y-0", "opacity-0");
-    mobileMenu.classList.remove("scale-y-100", "opacity-100");
-
-    // Wait for the transition to finish before hiding it
-    setTimeout(() => {
-      mobileMenu.classList.add("hidden");
-    }, 300); // Match this to the duration in the CSS transition
-  }
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
+
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
